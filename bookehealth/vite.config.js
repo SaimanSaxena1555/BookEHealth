bae64353f472a8d734337fb738130181cd6d87b2
Saiman Saxena <saimansaxena1555@gmail.com>
2026-01-15 17:59:54 +0530

    fix

--- bookehealth/vite.config.js
+++ bookehealth/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITA_BASE_PATH || '/BookEHealth/tree/main/bookehealth',
 })
